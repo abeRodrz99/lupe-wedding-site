1dc3612b6c49a6da264af5f0c4aa51f1e63add12
Abel Rodriguez <rodrz.abe@gmail.com>
2026-07-30 17:21:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -175,4 +175,4 @@ initReveal();
 initRSVP(locale);
 initGallery();
 initFAQ(locale);
-initcountdown({ targetIso: calendarEvent.startIso, locale });
+initCountdown({ targetIso: calendarEvent.startIso, locale });
